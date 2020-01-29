05e27555acd94abc0922230b163ace64d291f6fd
Lance Albertson <lance@osuosl.org>
2020-01-29 14:54:51 -0800

    Revert "Cinc branded chef-zero"
    This reverts commit b442301e59d968ad6fbbcb8a9d5efaf51e419098.

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -54,7 +54,6 @@ dependency "bundler"
 dependency "ohai"
 dependency "appbundler"
 dependency "libarchive" # for archive resource
-dependency "chef-zero"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
