0e4e679d81f103996b6b428e57214e334aafa6fa
Lance Albertson <lance@osuosl.org>
2020-01-29 14:54:51 -0800

    Revert "Cinc branded chef-zero"
    This reverts commit b442301e59d968ad6fbbcb8a9d5efaf51e419098.

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -50,7 +50,6 @@ relative_path "chef"
 
 dependency "ruby"
 dependency "libarchive" # for archive resource
-dependency "chef-zero"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
