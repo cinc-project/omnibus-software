7828b781c183e038d0c310976263cd45e3bbf568
Lance Albertson <lance@osuosl.org>
2024-12-28 12:09:51 -0800

    Set tag to match version of openssl gem
    This will fix issues when upstream updates the gem version.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -338,9 +338,7 @@ build do
     # add OPENSSL_FIPS to the environment _if_ fips is active
     fips_env = fips_mode? ? env.merge({ "OPENSSL_FIPS" => "1" }) : env
 
-    command "git clone https://github.com/ruby/openssl.git", cwd: "#{install_dir}"
-    # Checkout the specific tag for version 3.3.0
-    command "cd #{install_dir}/openssl && git checkout tags/v3.3.0"
+    command "git clone --branch v#{openssl_gem_version} https://github.com/ruby/openssl.git", cwd: "#{install_dir}"
     command "gem build openssl.gemspec", cwd: "#{install_dir}/openssl"
     command "gem install openssl-#{openssl_gem_version}.gem --no-document -- --with-openssl-dir=#{install_dir}/embedded", env: fips_env, cwd: "#{install_dir}/openssl"
 
