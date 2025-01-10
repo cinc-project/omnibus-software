18b69abb4ae07968441fef1d07fd08f1dc995269
Lance Albertson <lance@osuosl.org>
2025-01-09 16:19:59 -0800

    server-open-jre: update license file location
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/server-open-jre.rb
+++ config/software/server-open-jre.rb
@@ -25,7 +25,7 @@ end
 
 license "GPL-2.0 (with the Classpath Exception)"
 
-license_file "http://openjdk.java.net/legal/gplv2+ce.html"
+license_file "https://github.com/adoptium/jdk/raw/refs/heads/master/LICENSE"
 skip_transitive_dependency_licensing true
 
 whitelist_file "jre/bin/javaws"
