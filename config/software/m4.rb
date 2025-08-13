1c7ea86c71a0dcb9c0e0a055f417be0135e04385
Lance Albertson <lance@osuosl.org>
2025-08-13 12:03:36 -0700

    Update to use ftpmirror.gnu.org
    Their upstream mirror is having problems.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/m4.rb
+++ config/software/m4.rb
@@ -29,7 +29,7 @@ skip_transitive_dependency_licensing true
 version("1.4.20") { source sha256: "6ac4fc31ce440debe63987c2ebbf9d7b6634e67a7c3279257dc7361de8bdb3ef" }
 version("1.4.18") { source sha256: "ab2633921a5cd38e48797bf5521ad259bdc4b979078034a3b790d7fec5493fab" }
 
-source url: "https://ftp.gnu.org/gnu/m4/m4-#{version}.tar.gz"
+source url: "https://ftpmirror.gnu.org/gnu/m4/m4-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/m4-#{version}.tar.gz",
                 authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 relative_path "m4-#{version}"
--- config/software/ncurses.rb
+++ config/software/ncurses.rb
@@ -24,7 +24,7 @@ skip_transitive_dependency_licensing true
 
 dependency "config_guess"
 
-# versions_list: https://ftp.gnu.org/gnu/ncurses/ filter=*.tar.gz
+# versions_list: https://ftpmirror.gnu.org/gnu/ncurses/ filter=*.tar.gz
 version("6.5") { source sha256: "136d91bc269a9a5785e5f9e980bc76ab57428f604ce3e5a5a90cebc767971cc6" }
 version("6.4") { source sha256: "6931283d9ac87c5073f30b6290c4c75f21632bb4fc3603ac8100812bed248159" }
 version("6.3") { source sha256: "97fc51ac2b085d4cde31ef4d2c3122c21abc217e9090a43a30fc5ec21684e059" }
@@ -32,7 +32,7 @@ version("6.2") { source sha256: "30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227
 version("6.1") { source sha256: "aa057eeeb4a14d470101eff4597d5833dcef5965331be3528c08d99cebaa0d17" }
 version("5.9") { source sha256: "9046298fb440324c9d4135ecea7879ffed8546dd1b58e59430ea07a4633f563b" }
 
-source url: "https://mirror.team-cymru.com/gnu/ncurses/ncurses-#{version}.tar.gz"
+source url: "https://ftpmirror.gnu.org/gnu/ncurses/ncurses-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
                 authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 
