cccb30f4f834813bf6439bc96d9a1dc151028467
Lance Albertson <lance@osuosl.org>
2025-08-25 13:57:58 -0700

    runit: Add 2.2.0
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/runit.rb
+++ config/software/runit.rb
@@ -23,6 +23,7 @@ skip_transitive_dependency_licensing true
 
 # version_list: url=http://smarden.org/runit/ filter=*.tar.gz
 
+version("2.2.0") { source sha256: "95ef4d2868b978c7179fe47901e5c578e11cf273d292bd6208bd3a7ccb029290" }
 version("2.1.2") { source sha256: "6fd0160cb0cf1207de4e66754b6d39750cff14bb0aa66ab49490992c0c47ba18" }
 version("2.1.1") { source sha256: "ffcf2d27b32f59ac14f2d4b0772a3eb80d9342685a2042b7fbbc472c07cf2a2c" }
 
