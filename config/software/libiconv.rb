118d8e8f6a6c50d04d603cc1954d6a1d00a75634
Lance Albertson <lance@osuosl.org>
2026-03-18 13:24:46 -0700

    Update libiconv to 1.19
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/libiconv.rb
+++ config/software/libiconv.rb
@@ -19,7 +19,7 @@
 # external gem handling when chef-client is upgraded libconv could be dropped.
 #
 name "libiconv"
-default_version "1.17"
+default_version "1.19"
 
 license "LGPL-2.1"
 license_file "COPYING.LIB"
@@ -28,6 +28,7 @@ skip_transitive_dependency_licensing true
 dependency "config_guess"
 
 # versions_list: https://ftp.osuosl.org/pub/gnu/libiconv/ filter=*.tar.gz
+version("1.19") { source sha256: "88dd96a8c0464eca144fc791ae60cd31cd8ee78321e67397e25fc095c4a19aa6" }
 version("1.18") { source sha256: "3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8" }
 version("1.17") { source sha256: "8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313" }
 version("1.16") { source sha256: "e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04" }
