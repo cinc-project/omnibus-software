7bb021386ce95742d539cece0dae433ca554ac2a
Lance Albertson <lance@osuosl.org>
2026-03-18 13:36:56 -0700

    Update libxslt to 1.1.45
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/libxslt.rb
+++ config/software/libxslt.rb
@@ -15,7 +15,7 @@
 #
 
 name "libxslt"
-default_version "1.1.39"
+default_version "1.1.45"
 
 license "MIT"
 license_file "COPYING"
@@ -26,6 +26,7 @@ dependency "liblzma"
 dependency "config_guess"
 
 # versions_list: url=https://download.gnome.org/sources/libxslt/1.1/ filter=*.tar.xz
+version("1.1.45") { source sha256: "9acfe68419c4d06a45c550321b3212762d92f41465062ca4ea19e632ee5d216e" }
 version("1.1.43") { source sha256: "5a3d6b383ca5afc235b171118e90f5ff6aa27e9fea3303065231a6d403f0183a" }
 version("1.1.42") { source sha256: "85ca62cac0d41fc77d3f6033da9df6fd73d20ea2fc18b0a3609ffb4110e1baeb" }
 version("1.1.39") { source sha256: "2a20ad621148339b0759c4d4e96719362dee64c9a096dbba625ba053846349f0" }
