2ad31cdfb399692cda5a99a131cb6a960335b759
Lance Albertson <lance@osuosl.org>
2026-03-18 13:19:15 -0700

    Update cacerts to 2025-12-02
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -20,13 +20,14 @@ license "MPL-2.0"
 license_file "https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt"
 skip_transitive_dependency_licensing true
 
-default_version "2025-09-09"
+default_version "2025-12-02"
 
 source url: "https://curl.se/ca/cacert-#{version}.pem"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/cacert-#{version}.pem",
                 authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 
 # versions_list: https://curl.se/docs/caextract.html
+version("2025-12-02") { source sha256: "f1407d974c5ed87d544bd931a278232e13925177e239fca370619aba63c757b4" }
 version("2025-09-09") { source sha256: "f290e6acaf904a4121424ca3ebdd70652780707e28e8af999221786b86bb1975" }
 version("2025-05-20") { source sha256: "ab3ee3651977a4178a702b0b828a4ee7b2bbb9127235b0ab740e2e15974bf5db" }
 version("2022-10-11") { source sha256: "2cff03f9efdaf52626bd1b451d700605dc1ea000c5da56bd0fc59f8f43071040" }
