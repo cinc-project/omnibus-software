876168a7298d9dcc868ce1c8be8acdbb997bc68c
Lance Albertson <lance@osuosl.org>
2026-04-06 16:14:14 -0700

    Add sha256 checksum for erlang 28.4.1

--- config/software/erlang.rb
+++ config/software/erlang.rb
@@ -34,6 +34,7 @@ relative_path "otp-OTP-#{version}"
 
 # versions_list: https://github.com/erlang/otp/tags filter=*.tar.gz
 # to get the SHA256, download the tar.gz, then calculate the SHA256 on it
+version("28.4.1") { source sha256: "a256e77201009050ef55602a57154614c215f611fda9f0fcda991bb647aa9fd9" }
 version("26.2.5.14") { source sha256: "5378dc60382c3d43ecdc0e8666c5db0f8a1df1525fff706779f720ad1d54c56c" }
 version("26.2.5.2")  { source sha256: "8e537e2d984770796cc7f0c7c079a9e5fbc67b8c368e0dcd9aa2ceaeb2844da2" }
 version("25.2")      { source sha256: "d33a988f39e534aff67799c5b9635612858459c9d8890772546d71ea38de897a" }
