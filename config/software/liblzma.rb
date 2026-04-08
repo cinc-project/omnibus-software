b6c1e9554610bcda0d18a19f513cdc02b40351f5
Lance Albertson <lance@osuosl.org>
2026-04-08 11:19:29 -0700

    Remove old liblzma versions, keep 5.8.3
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/liblzma.rb
+++ config/software/liblzma.rb
@@ -24,16 +24,6 @@ skip_transitive_dependency_licensing true
 # version_list: url=http://tukaani.org/xz/ filer=*.tar.gz
 
 version("5.8.3") { source sha256: "3d3a1b973af218114f4f889bbaa2f4c037deaae0c8e815eec381c3d546b974a0" }
-version("5.8.2") { source sha256: "ce09c50a5962786b83e5da389c90dd2c15ecd0980a258dd01f70f9e7ce58a8f1" }
-version("5.8.1") { source sha256: "507825b599356c10dca1cd720c9d0d0c9d5400b9de300af00e4d1ea150795543" }
-version("5.4.7") { source sha256: "8db6664c48ca07908b92baedcfe7f3ba23f49ef2476864518ab5db6723836e71" }
-version("5.2.10") { source sha256: "eb7a3b2623c9d0135da70ca12808a214be9c019132baaa61c9e1d198d1d9ded3" }
-version("5.2.7") { source sha256: "06327c2ddc81e126a6d9a78b0be5014b976a2c0832f492dcfc4755d7facf6d33" }
-version("5.2.6") { source sha256: "a2105abee17bcd2ebd15ced31b4f5eda6e17efd6b10f921a01cda4a44c91b3a0" }
-version("5.2.5") { source sha256: "f6f4910fd033078738bd82bfba4f49219d03b17eb0794eb91efbae419f4aba10" }
-version("5.2.4") { source sha256: "b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145" }
-version("5.2.3") { source sha256: "71928b357d0a09a12a4b4c5fafca8c31c19b0e7d3b8ebb19622e96f26dbf28cb" }
-version("5.2.2") { source sha256: "73df4d5d34f0468bd57d09f2d8af363e95ed6cc3a4a86129d2f2c366259902a2" }
 
 source url: "https://tukaani.org/xz/xz-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/xz-#{version}.tar.gz",
