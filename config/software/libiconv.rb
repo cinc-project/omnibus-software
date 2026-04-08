39d0dacfce120b7fefc0909315c32f7da1d669d5
Lance Albertson <lance@osuosl.org>
2026-04-08 11:57:17 -0700

    Remove old libiconv versions, keep 1.19
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/libiconv.rb
+++ config/software/libiconv.rb
@@ -29,10 +29,6 @@ dependency "config_guess"
 
 # versions_list: https://ftp.osuosl.org/pub/gnu/libiconv/ filter=*.tar.gz
 version("1.19") { source sha256: "88dd96a8c0464eca144fc791ae60cd31cd8ee78321e67397e25fc095c4a19aa6" }
-version("1.18") { source sha256: "3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8" }
-version("1.17") { source sha256: "8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313" }
-version("1.16") { source sha256: "e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04" }
-version("1.15") { source sha256: "ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178" }
 
 source url: "https://ftp.osuosl.org/pub/gnu/libiconv/libiconv-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
