e0edacd49e7cd81686a048434bdb17b2a3abfae8
Lance Albertson <lance@osuosl.org>
2026-04-09 10:48:32 -0700

    automake: update to 1.18.1, remove deprecated marker
    - Update from 1.16 to 1.18.1
    - Remove old versions 1.15 and 1.11.2
    - Remove version-conditional bootstrap.sh/bootstrap logic
    - Remove deprecated marker so upstream checker can track updates
    - Add version_list comment
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/automake.rb
+++ config/software/automake.rb
@@ -13,10 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# expeditor/ignore: deprecated 2021-04
+# version_list: url=https://ftp.osuosl.org/pub/gnu/automake/ filter=automake-*.tar.gz
 
 name "automake"
-default_version "1.16"
+default_version "1.18.1"
 
 dependency "autoconf"
 dependency "perl-thread-queue"
@@ -25,9 +25,7 @@ license "GPL-2.0"
 license_file "COPYING"
 skip_transitive_dependency_licensing true
 
-version("1.16") { source sha256: "80da43bb5665596ee389e6d8b64b4f122ea4b92a685b1dbd813cd1f0e0c2d83f" }
-version("1.15") { source sha256: "7946e945a96e28152ba5a6beb0625ca715c6e32ac55f2e353ef54def0c8ed924" }
-version("1.11.2") { source sha256: "c339e3871d6595620760725da61de02cf1c293af8a05b14592d6587ac39ce546" }
+version("1.18.1") { source sha256: "63e585246d0fc8772dffdee0724f2f988146d1a3f1c756a3dc5cfbefa3c01915" }
 
 source url: "https://ftp.osuosl.org/pub/gnu/automake/automake-#{version}.tar.gz"
 
@@ -36,11 +34,6 @@ relative_path "automake-#{version}"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  if version == "1.15"
-    command "./bootstrap.sh", env: env
-  else
-    command "./bootstrap", env: env
-  end
   command "./configure" \
           " --prefix=#{install_dir}/embedded", env: env
 
