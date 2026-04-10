e88f87ad1b1a69031ee06ee07334bd1619fa716a
Lance Albertson <lance@osuosl.org>
2026-04-09 22:38:50 -0700

    Clean up makedepend: keep only latest version 1.0.9
    - Remove older version definitions (1.0.8, 1.0.6, 1.0.5)
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/makedepend.rb
+++ config/software/makedepend.rb
@@ -24,9 +24,6 @@ skip_transitive_dependency_licensing true
 # version_list: url=https://www.x.org/releases/individual/util/ Filter=makedepend-*.tar.gz
 
 version("1.0.9") { source sha256: "bc94ffda6cd4671603a69c39dbe8f96b317707b9185b2aaa3b54b5d134b41884" }
-version("1.0.8") { source sha256: "275f0d2b196bfdc740aab9f02bb48cb7a97e4dfea011a7b468ed5648d0019e54" }
-version("1.0.6") { source sha256: "845f6708fc850bf53f5b1d0fb4352c4feab3949f140b26f71b22faba354c3365" }
-version("1.0.5") { source sha256: "503903d41fb5badb73cb70d7b3740c8b30fe1cc68c504d3b6a85e6644c4e5004" }
 
 source url: "https://www.x.org/releases/individual/util/makedepend-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
