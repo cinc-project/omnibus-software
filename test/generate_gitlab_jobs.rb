7cbd403d5b43372e740f125108a75ae5820597ca
Lance Albertson <lance@osuosl.org>
2026-04-12 12:07:06 -0700

    Add musl to HEALTH_CHECK_SKIP_LIST
    - Omnibus health check fails with 'no good libraries were found'
    - musl produces a libc that the health check does not recognize
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/generate_gitlab_jobs.rb
+++ test/generate_gitlab_jobs.rb
@@ -6,7 +6,7 @@ require "yaml"
 BRANCH = "stable/cinc".freeze
 
 # Skip health check when it is not relevant
-HEALTH_CHECK_SKIP_LIST = %w{ cacerts config_guess xproto util-macros }.freeze
+HEALTH_CHECK_SKIP_LIST = %w{ cacerts config_guess xproto util-macros musl }.freeze
 DEPRECATED_SKIP_LIST = %w{ git-windows cmake ruby-msys2-devkit }.freeze
 OPENSSL_VALIDATION_TYPES = %w{ executable ruby providers }.freeze
 
