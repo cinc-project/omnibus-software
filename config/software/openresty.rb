fd1d9f5b9e68108c2ec6ef29f7b9bd95e15f58bf
Lance Albertson <lance@osuosl.org>
2026-04-12 12:03:30 -0700

    Clean up openresty: update to latest version 1.29.2.3
    - Update default version from 1.27.1.2 to 1.29.2.3
    - Remove older version definitions
    - Remove dead ppc64/ppc64le/s390x Lua fallback code (always exited for versions >= 1.13)
    - Remove lua dependency (no longer needed)
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- config/software/openresty.rb
+++ config/software/openresty.rb
@@ -18,25 +18,14 @@ name "openresty"
 license "BSD-2-Clause"
 license_file "README.markdown"
 skip_transitive_dependency_licensing true
-default_version "1.27.1.2"
+default_version "1.29.2.3"
 
 dependency "pcre"
 dependency "openssl"
 dependency "zlib"
-dependency "lua" if ppc64? || ppc64le? || s390x?
 
 # versions_list: https://openresty.org/download/ filter=*.tar.gz
-version("1.27.1.2")    { source sha256: "74f076f7e364b2a99a6c5f9bb531c27610c78985abe956b442b192a2295f7548" }
-version("1.25.3.1")    { source sha256: "32ec1a253a5a13250355a075fe65b7d63ec45c560bbe213350f0992a57cd79df" }
-version("1.21.4.2")    { source sha256: "5b1eded25c1d4ed76c0336dfae50bd94d187af9c85ead244135dd5ae363b2e2a" }
-version("1.21.4.1")    { source sha256: "0c5093b64f7821e85065c99e5d4e6cc31820cfd7f37b9a0dec84209d87a2af99" }
-version("1.21.4.1rc1") { source sha256: "1cb216bc57a253149cb5c4b815bfe00e1c0713e7355774bbc26cf306d2ff632f" }
-version("1.19.9.1")    { source sha256: "576ff4e546e3301ce474deef9345522b7ef3a9d172600c62057f182f3a68c1f6" }
-version("1.19.3.2")    { source sha256: "ce40e764990fbbeb782e496eb63e214bf19b6f301a453d13f70c4f363d1e5bb9" }
-version("1.19.3.1")    { source sha256: "f36fcd9c51f4f9eb8aaab8c7f9e21018d5ce97694315b19cacd6ccf53ab03d5d" }
-version("1.17.8.2")    { source sha256: "2f321ab11cb228117c840168f37094ee97f8f0316eac413766305409c7e023a0" }
-version("1.15.8.1")    { source sha256: "89a1238ca177692d6903c0adbea5bdf2a0b82c383662a73c03ebf5ef9f570842" }
-version("1.13.6.2")    { source sha256: "946e1958273032db43833982e2cec0766154a9b5cb8e67868944113208ff2942" }
+version("1.29.2.3") { source sha256: "315e49fa4568747fec4bdada9614d2ba287e7aed4b3430d7ea25685e24cc43ff" }
 
 source url: "https://openresty.org/download/openresty-#{version}.tar.gz"
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
@@ -77,17 +66,7 @@ build do
     # '--with-libatomic'
   ]
 
-  # Currently LuaJIT doesn't support POWER correctly so use Lua51 there instead
-  if ppc64? || ppc64le? || s390x?
-    # 1.13 breaks these; discover a workaround
-    if version.satisfies?(">= 1.13")
-      exit("ppc64? || ppc64le? || s390x? unsupported for 1.13 and greater")
-    else
-      configure << "--with-lua51=#{install_dir}/embedded/lib"
-    end
-  else
-    configure << "--with-luajit"
-  end
+  configure << "--with-luajit"
 
   command configure.join(" "), env: env
 
